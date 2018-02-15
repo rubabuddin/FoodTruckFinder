7c0df85d644010ff182cafa5f26fdf3939aab385
Rubab Uddin <uddin.rubab@gmail.com>
2018-02-15 00:37:24 -0800

    cleanup

--- FoodTruckFinder.java
+++ FoodTruckFinder.java
@@ -19,11 +19,11 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 public class FoodTruckFinder {
-	
+
 	public static List<FoodTruck> foodTruckData  = new ArrayList<FoodTruck>();
-	
+
 	public static void main(String[] args) {
-		
+
 		//get data from sfgov.org food truck data API
 		JsonArray foodTruckAPIData = new JsonArray();
 		try {
@@ -31,22 +31,21 @@ public class FoodTruckFinder {
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
 		}
-		
+
 		//store the food trucks that are currently open inside a list
 		populateFoodTruckArray(foodTruckAPIData);
-		
+
 		//sort the list alphabetically
 		sortFoodTruckList();
-		
+
 		//iterate through list and print as output
 		printList();
 	}
 
-	//ArrayList<ArrayList<String,String>> throws JsonIOException, JsonSyntaxException, IOException
 	public static JsonArray getAPIData() throws IOException {
 
 		String dayOfWeek = getDayOfWeek();
-		//format JSON response to get desired
+		//format JSON response to get desired dayOfWeek
 		String sURL = "http://data.sfgov.org/resource/bbb8-hzi6.json" + "?dayofweekstr=" + dayOfWeek;
 
 		URL url = new URL(sURL);
@@ -54,80 +53,77 @@ public class FoodTruckFinder {
 		request.connect();
 		if(request.getResponseCode() != 200)
 			System.out.println("API Access Failed - data invalid");
-	
+
 		//Convert data to JSON object
 		JsonParser jp = new JsonParser(); //from gson
 
 		JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent())); //Convert the input stream
 		return root.getAsJsonArray();
 
-	}	
-	
+	}
+
 	public static void populateFoodTruckArray(JsonArray rootobj){
-		
+
 		//iterate and store in array of class FoodTruck
 		for(int i=0; i<rootobj.size(); i++){
-			
+
 			JsonObject foodTruckJsonObject = rootobj.get(i).getAsJsonObject();
 			//create new FoodTruck object
 			FoodTruck foodTruck = new FoodTruck();
-			
+
 			//API Details
 			//	name of restaurant is "applicant"
 			// 	address of restaurant is "location"
 			//	time open: "start24" < currenTime < "end24"
-			
+
 			foodTruck.name = foodTruckJsonObject.get("applicant").getAsString();
-			foodTruck.location = foodTruckJsonObject.get("location").getAsString();	
+			foodTruck.location = foodTruckJsonObject.get("location").getAsString();
 			String startTime = foodTruckJsonObject.get("start24").getAsString();
 			String endTime = foodTruckJsonObject.get("end24").getAsString();
-			
+
 			boolean openNow = isFoodTruckOpen(startTime, endTime);
 			foodTruck.openNow = openNow;
-			
+
 			//only add to our list if it is open right now
 			if(openNow){
 				foodTruckData.add(foodTruck);
-			}	
+			}
 		}
 	}
-	
+
 	private static void sortFoodTruckList() {
 		Collections.sort(foodTruckData, new Comparator<FoodTruck>() {
-
 	        public int compare(FoodTruck f1, FoodTruck f2) {
 	            return f1.getName().compareTo(f2.getName());
 	        }
 	    });
-		
 	}
-	
+
 	private static void printList() {
-		// TODO Auto-generated method stub
+		//format for cleaner output
 		String format = "%-80s%s%n";
-	
-		
+
 		int dataSetSize = foodTruckData.size()+1;
 		int currentPage = 0;
 		int totalPages = dataSetSize/10;
 		if(totalPages >= 1 && dataSetSize%10 != 0){
 			totalPages++;
 		}
-		int counter = 0; //use to show 10 foodtrucks at a time;
-		
+		int counter = 0; //use to show 10 foodtrucks at a time
+
 		for(int i=0; i<foodTruckData.size(); i++){
 			if(counter == 0){
 				currentPage++;
 				System.out.println("\nFood Trucks Currently Open in San Francisco - Page " + currentPage + " of " + totalPages);
 				System.out.println();
 				System.out.printf(format, "NAME", "ADDRESS");
-				
+
 			}
 			FoodTruck foodTruck = foodTruckData.get(i);
 			System.out.printf(format, foodTruck.name, foodTruck.location);
-		
+
 			counter++;
-			//if we hit 10 visible food truck entries, wait for user input
+			//if we hit 10 visible food truck entries, wait for user input before showing more
 			if(counter == 10){
 				waitForUserInput();
 				//clear screen
@@ -143,11 +139,10 @@ public class FoodTruckFinder {
 		try {
 			System.in.read();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 	private static String getDayOfWeek(){
 		Calendar calendar = Calendar.getInstance();
 		Date date = calendar.getTime();
@@ -160,22 +155,21 @@ public class FoodTruckFinder {
 	private static boolean isFoodTruckOpen(String startTime, String endTime) {
 		DateFormat df = new SimpleDateFormat("HH:mm");
 		String now = df.format(new Date());
-		
+
 		try{
 			Date open = df.parse(startTime);
 			Date close = df.parse(endTime);
 			Date currentTime = df.parse(now);
-			
-			
+
 			if(currentTime.after(open) && currentTime.before(close))
 				return true;
 			else
 				return false;
-		
+
 		} catch (Exception e) {
 			e.printStackTrace();;
 		}
-		
+
 		return false;
 	}
 
